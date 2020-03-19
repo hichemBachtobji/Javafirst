098d672bdeafdfab9e4d145a20b2fdc2deb7931b
hichemBachtobji <bachtobji.hichem@gmail.com>
2020-03-18 20:32:34 -0400

    writing code on class 13

--- src/com/syntax/class13/Q1.java
+++ src/com/syntax/class13/Q1.java
@@ -3,8 +3,35 @@ package com.syntax.class13;
 public class Q1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/*
+	 * Write a program to swap 2 numbers without a temporary variable?
+	 * swap 2 strings without a temporary variable?
+	 */
 
+		int a=10;
+		int b=20;
+		
+		a=a+b;//30
+		b=a-b;//30-20=10
+		a=a-b;
+		System.out.println("the value of a: "+a);
+		System.out.println("the value of b: "+b);
+		
+		String str="Day";
+		String str2="night";
+		str=str+str2;//dayNight
+		str2=str.substring(0,3);// day
+		str=str.substring(3);//night
+		System.out.println("the value of str: "+str);
+		System.out.println("the value of str2: "+str2);
+		
+		// another way
+		String s1="Hello";
+		String s2="bye";
+		
+		String shorter =s1+s2;
+		s2=shorter
+		
 	}
 
 }
